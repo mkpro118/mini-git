b2a103094f5ca224e997dcd9d40c75c7720f3d4c
M K <mkpro118@gmail.com>
2024-07-07 16:56:46 -0500

    Added digest size constant

--- src/sha1.rs
+++ src/sha1.rs
@@ -1,10 +1,13 @@
-const SHA1_INIT_DIGEST: [u32; 5] = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0];
-
+pub const DIGEST_SIZE_BYTES: usize = 20;
 const SIZE_OF_U32: usize = 32;
+const DIGEST_SIZE: usize = 5;
+
+const SHA1_INIT_DIGEST: [u32; DIGEST_SIZE] =
+    [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0];
 
 #[derive(Debug)]
 pub struct SHA1 {
-    digest: [u32; 5],
+    digest: [u32; DIGEST_SIZE],
     buffer: String,
     transforms: usize,
 }
