e9e3d0c8fcbf7c89a16215478137831ec87c634d
M K <mkpro118@gmail.com>
2024-07-07 17:33:09 -0500

    Reorganize file

--- src/sha1.rs
+++ src/sha1.rs
@@ -12,14 +12,6 @@ pub struct SHA1 {
     transforms: usize,
 }
 
-fn rotate_left(uint: u32, shift: usize) -> u32 {
-    uint << shift | uint >> (SIZE_OF_U32 - shift)
-}
-
-trait Hash<T> {
-    fn hash(&self, message: T);
-}
-
 impl SHA1 {
     fn new() -> Self {
         Self {
@@ -34,6 +26,18 @@ impl SHA1 {
     }
 }
 
+impl From<&str> for SHA1 {
+    fn from(text: &str) -> Self {
+        let sha = Self::new();
+        sha.hash(text);
+        sha
+    }
+}
+
+trait Hash<T> {
+    fn hash(&self, message: T);
+}
+
 impl Hash<&str> for SHA1 {
     fn hash(&self, message: &str) {
         self.hash(message.as_bytes())
@@ -44,10 +48,6 @@ impl Hash<&[u8]> for SHA1 {
     fn hash(&self, message: &[u8]) {}
 }
 
-impl From<&str> for SHA1 {
-    fn from(text: &str) -> Self {
-        let sha = Self::new();
-        sha.hash(text);
-        sha
-    }
+fn rotate_left(uint: u32, shift: usize) -> u32 {
+    uint << shift | uint >> (SIZE_OF_U32 - shift)
 }
