73a08cc7f9a4cfba4ccc7f19b9444363b0cb17d2
M K <mkpro118@gmail.com>
2024-07-07 16:32:49 -0500

    Implemented sha1::new

--- src/sha1.rs
+++ src/sha1.rs
@@ -4,3 +4,13 @@ struct SHA1 {
     buffer: String,
     transforms: usize,
 }
+
+impl SHA1 {
+    fn new() -> Self {
+        Self {
+            digest: [0u32; 5],
+            buffer: String::new(),
+            transforms: 0usize,
+        }
+    }
+}
