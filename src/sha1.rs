1d7994d5a5bae5369c911345d9d91269b0074099
M K <mkpro118@gmail.com>
2024-07-07 17:28:53 -0500

    Template for SHA1

--- src/sha1.rs
+++ src/sha1.rs
@@ -16,6 +16,10 @@ fn rotate_left(uint: u32, shift: usize) -> u32 {
     uint << shift | uint >> (SIZE_OF_U32 - shift)
 }
 
+trait Hash<T> {
+    fn hash(&self, message: T);
+}
+
 impl SHA1 {
     fn new() -> Self {
         Self {
@@ -23,5 +27,27 @@ impl SHA1 {
             buffer: String::new(),
             transforms: 0usize,
         }
+    }
+
+    fn hex_digest(&self) -> String {
+        unimplemented!();
+    }
+}
+
+impl Hash<&str> for SHA1 {
+    fn hash(&self, message: &str) {
+        self.hash(message.as_bytes())
+    }
+}
+
+impl Hash<&[u8]> for SHA1 {
+    fn hash(&self, message: &[u8]) {}
+}
+
+impl From<&str> for SHA1 {
+    fn from(text: &str) -> Self {
+        let sha = Self::new();
+        sha.hash(text);
+        sha
     }
 }
