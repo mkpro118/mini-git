a0dfe6875b50d909fdf7836b7d07d7a4f06ceba2
M K <mkpro118@gmail.com>
2024-07-07 16:48:59 -0500

    Added constants and left-rotation

--- src/sha1.rs
+++ src/sha1.rs
@@ -1,14 +1,22 @@
+const SHA1_INIT_DIGEST: [u32; 5] = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0];
+
+const SIZE_OF_U32: usize = 32;
+
 #[derive(Debug)]
-struct SHA1 {
+pub struct SHA1 {
     digest: [u32; 5],
     buffer: String,
     transforms: usize,
 }
 
+fn rotate_left(uint: u32, shift: usize) -> u32 {
+    uint << shift | uint >> (SIZE_OF_U32 - shift)
+}
+
 impl SHA1 {
     fn new() -> Self {
         Self {
-            digest: [0u32; 5],
+            digest: SHA1_INIT_DIGEST,
             buffer: String::new(),
             transforms: 0usize,
         }
