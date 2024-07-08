ba08db2fec2ea5c0a0e80fbd605ac3be707b4bc0
M K <mkpro118@gmail.com>
2024-07-07 22:43:21 -0500

    Clippy compliant

--- src/sha1.rs
+++ src/sha1.rs
@@ -1,6 +1,13 @@
+use std::fmt::Write;
 use std::iter;
 
-const INITIAL_STATE: [u32; 5] = [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0];
+const INITIAL_STATE: [u32; 5] = [
+    0x6745_2301,
+    0xEFCD_AB89,
+    0x98BA_DCFE,
+    0x1032_5476,
+    0xC3D2_E1F0,
+];
 
 pub struct SHA1 {
     state: [u32; 5],
@@ -8,7 +15,14 @@ pub struct SHA1 {
     total_len: u64,
 }
 
+impl Default for SHA1 {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl SHA1 {
+    #[must_use]
     pub fn new() -> Self {
         SHA1 {
             state: INITIAL_STATE,
@@ -17,7 +31,8 @@ impl SHA1 {
         }
     }
 
-    pub fn update(&mut self, data: &[u8]) -> Self {
+    #[must_use]
+    pub fn update(&mut self, data: &[u8]) -> &mut Self {
         self.total_len += data.len() as u64;
         self.buffer.extend_from_slice(data);
 
@@ -33,13 +48,13 @@ impl SHA1 {
                     }
                 });
 
-        SHA1 {
-            state: new_state,
-            buffer: new_buffer,
-            total_len: self.total_len,
-        }
+        self.state = new_state;
+        self.buffer = new_buffer;
+
+        self
     }
 
+    #[allow(missing_docs)]
     pub fn finalize(&mut self) -> [u8; 20] {
         let mod_len = (self.total_len % 64) as usize;
         let padding = create_padding(mod_len, self.total_len);
@@ -55,10 +70,10 @@ impl SHA1 {
     }
 
     pub fn hex_digest(&mut self) -> String {
-        self.finalize()
-            .iter()
-            .map(|&byte| format!("{:02x}", byte))
-            .collect()
+        self.finalize().iter().fold(String::new(), |mut output, b| {
+            let _ = write!(output, "{b:02x}");
+            output
+        })
     }
 }
 
@@ -74,16 +89,17 @@ fn create_padding(mod_len: usize, total_len: u64) -> Vec<u8> {
     padding
 }
 
+#[allow(clippy::many_single_char_names)]
 fn process_chunk(chunk: &[u8], initial_state: [u32; 5]) -> [u32; 5] {
     let words = expand_chunk(chunk);
     let [a, b, c, d, e] = initial_state;
 
     let final_state = (0..80).fold((a, b, c, d, e), |(a, b, c, d, e), i| {
         let (f, k) = match i {
-            0..=19 => ((b & c) | ((!b) & d), 0x5A827999),
-            20..=39 => (b ^ c ^ d, 0x6ED9EBA1),
-            40..=59 => ((b & c) | (b & d) | (c & d), 0x8F1BBCDC),
-            60..=79 => (b ^ c ^ d, 0xCA62C1D6),
+            0..=19 => ((b & c) | ((!b) & d), 0x5A82_7999),
+            20..=39 => (b ^ c ^ d, 0x6ED9_EBA1),
+            40..=59 => ((b & c) | (b & d) | (c & d), 0x8F1B_BCDC),
+            60..=79 => (b ^ c ^ d, 0xCA62_C1D6),
             _ => unreachable!(),
         };
 
@@ -124,6 +140,7 @@ fn expand_chunk(chunk: &[u8]) -> [u32; 80] {
     words
 }
 
+#[must_use]
 pub fn hash(message: &[u8]) -> [u8; 20] {
     SHA1::new().update(message).finalize()
 }
