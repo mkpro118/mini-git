fbd22b10716c96efd98cb471f8d1c6932d05ad26
M K <mkpro118@gmail.com>
2024-07-07 19:57:19 -0500

    Implemented the SHA-1 Main Loop

--- src/sha1.rs
+++ src/sha1.rs
@@ -1,9 +1,15 @@
+use std::ops::BitXor;
+
 pub const DIGEST_SIZE_BYTES: usize = 20;
 const SIZE_OF_BYTE: usize = 8;
 const SIZE_OF_U32: usize = 32;
 const SIZE_OF_U64: usize = 64;
 const DIGEST_SIZE: usize = 5;
 const CHUNK_SIZE: usize = 512;
+const ROUND1_K: u32 = 0x5A82_7999;
+const ROUND2_K: u32 = 0x6ED9_EBA1;
+const ROUND3_K: u32 = 0x8F1B_BCDC;
+const ROUND4_K: u32 = 0xCA62_C1D6;
 
 const SHA1_INIT_DIGEST: [u32; DIGEST_SIZE] =
     [0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0];
@@ -90,6 +96,119 @@ fn preprocess_message(message: &[u8]) -> Vec<u8> {
     seq.extend_from_slice(&message_length_as_bytes);
 
     seq
+}
+
+fn hash_pre_processed(message: &[u8]) -> [u32; 20] {
+    assert!(message.len() >= 512, "Message is too short");
+    assert_eq!(message.len() % 512, 0, "Message is not a multiple of 512");
+
+    // Break message into 512 bit chunks
+    let digest = message.chunks(CHUNK_SIZE / SIZE_OF_BYTE).fold(
+        SHA1_INIT_DIGEST,
+        |digest: [u32; 5], chunk: &[u8]| {
+            // Convert a chunk of 4 bytes (u8) to a word (u32)
+            let chunk: [u32; 16] = chunk
+                .chunks(SIZE_OF_U32 / SIZE_OF_BYTE)
+                .map(|sub_chunk: &[u8]| {
+                    let bytes: [u8; 4] = sub_chunk.try_into().unwrap();
+                    u32::from_be_bytes(bytes)
+                })
+                .collect::<Vec<u32>>()
+                .try_into()
+                .unwrap();
+
+            // Hash the chunk
+            hash_chunk(&digest, &chunk)
+        },
+    );
+
+    [0; 20]
+}
+
+fn hash_chunk(digest: &[u32; 5], chunk: &[u32; 16]) -> [u32; 5] {
+    let schedule = message_schedule(chunk);
+    sha1_main(digest, &schedule)
+}
+
+fn message_schedule(chunk: &[u32; 16]) -> [u32; 80] {
+    let mut words = [0u32; 80];
+
+    // The first 16 words are from the chunk
+    words[..chunk.len()].clone_from_slice(chunk);
+
+    // Populate the rest of the words
+    for i in chunk.len()..words.len() {
+        words[i] = words[i - 3]
+            .bitxor(words[i - 8])
+            .bitxor(words[i - 14])
+            .bitxor(words[i - 16]);
+        words[i] = rotate_left(words[i], 1);
+    }
+
+    words
+}
+
+fn sha1_main(hash_vars: &[u32; 5], schedule: &[u32; 80]) -> [u32; 5] {
+    let r1 = round1(hash_vars, schedule[..20].try_into().unwrap());
+    let r2 = round2_or_4(&r1, schedule[20..40].try_into().unwrap(), true);
+    let r3 = round3(&r2, schedule[40..60].try_into().unwrap());
+    let r4 = round2_or_4(&r3, schedule[60..80].try_into().unwrap(), false);
+
+    hash_vars
+        .iter()
+        .zip(r4.iter())
+        .map(|(x, y)| x.wrapping_add(*y))
+        .collect::<Vec<u32>>()
+        .try_into()
+        .unwrap()
+}
+
+#[inline]
+fn round1(hash_vars: &[u32; 5], words: &[u32; 20]) -> [u32; 5] {
+    let [mut a, mut b, mut c, mut d, mut e] = hash_vars;
+
+    for &word in words {
+        let f = (b & c) | ((!b) & d);
+        [a, b, c, d, e] = swap_vars(word, [a, b, c, d, e], f, ROUND1_K);
+    }
+
+    [a, b, c, d, e]
+}
+
+#[inline]
+fn round2_or_4(hash_vars: &[u32; 5], words: &[u32; 20], r2: bool) -> [u32; 5] {
+    let [mut a, mut b, mut c, mut d, mut e] = hash_vars;
+    let k: u32 = if r2 { ROUND2_K } else { ROUND4_K };
+
+    for &word in words {
+        let f = b ^ c ^ d;
+        [a, b, c, d, e] = swap_vars(word, [a, b, c, d, e], f, k);
+    }
+
+    [a, b, c, d, e]
+}
+
+#[inline]
+fn round3(hash_vars: &[u32; 5], words: &[u32; 20]) -> [u32; 5] {
+    let [mut a, mut b, mut c, mut d, mut e] = hash_vars;
+
+    for &word in words {
+        let f = (b & c) | (b & d) | (c & d);
+        [a, b, c, d, e] = swap_vars(word, [a, b, c, d, e], f, ROUND3_K);
+    }
+
+    [a, b, c, d, e]
+}
+
+#[inline]
+fn swap_vars(word: u32, vars: [u32; 5], f: u32, k: u32) -> [u32; 5] {
+    let temp = rotate_left(vars[0], 5)
+        .wrapping_add(f)
+        .wrapping_add(vars[4])
+        .wrapping_add(k)
+        .wrapping_add(word);
+
+    [temp, vars[0], rotate_left(vars[1], 30), vars[2], vars[3]]
 }
 
 fn rotate_left(uint: u32, shift: usize) -> u32 {
