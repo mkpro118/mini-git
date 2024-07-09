24fd253f0f59857f0ec7395f2124ca9ab0db8262
M K <mkpro118@gmail.com>
2024-07-08 19:49:06 -0500

    Allow simple indexing on both structs

--- src/configparser.rs
+++ src/configparser.rs
@@ -1,4 +1,5 @@
-use std::collections::HashMap;
+use core::ops::Index;
+use std::{collections::HashMap, ops::IndexMut};
 
 #[derive(Debug)]
 struct ConfigSection {
@@ -24,6 +25,22 @@ impl Default for ConfigSection {
     }
 }
 
+impl Index<&str> for ConfigSection {
+    type Output = String;
+
+    fn index(&self, index: &str) -> &Self::Output {
+        &self.configs[index]
+    }
+}
+
+impl IndexMut<&str> for ConfigSection {
+    fn index_mut(&mut self, index: &str) -> &mut Self::Output {
+        self.configs
+            .get_mut(index)
+            .expect("should be able to add key")
+    }
+}
+
 impl ConfigParser {
     pub fn new() -> Self {
         Self {
@@ -35,5 +52,21 @@ impl ConfigParser {
 impl Default for ConfigParser {
     fn default() -> Self {
         Self::new()
+    }
+}
+
+impl Index<&str> for ConfigParser {
+    type Output = ConfigSection;
+
+    fn index(&self, index: &str) -> &Self::Output {
+        &self.sections[index]
+    }
+}
+
+impl IndexMut<&str> for ConfigParser {
+    fn index_mut(&mut self, index: &str) -> &mut Self::Output {
+        self.sections
+            .get_mut(index)
+            .expect("should be able to add key")
     }
 }
