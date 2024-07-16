34a1b6034671b968deb2f0d95048306b43ebc4d8
M K <mkpro118@gmail.com>
2024-07-16 16:00:46 -0500

    Documentation for cmd_init

--- src/core/init.rs
+++ src/core/init.rs
@@ -3,6 +3,18 @@ use std::path::Path;
 
 const DEFAULT_PATH: &str = ".";
 
+/// Initializes a new git repository
+/// This handles the subcommand
+///
+/// ```bash
+/// mini_git init [path]
+/// ```
+///
+/// # Errors
+///
+/// If file system operations fail, or if input paths are not valid.
+/// A [`String`] message describing the error is returned.
+#[allow(clippy::module_name_repetitions)]
 pub fn cmd_init(
     args: impl IntoIterator<Item = impl AsRef<str>>,
 ) -> Result<String, String> {
