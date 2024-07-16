ae1883b2fa0c671f98f839daeaf9c6e7d33a2c0b
M K <mkpro118@gmail.com>
2024-07-16 16:13:57 -0500

    Add help for the init command

--- src/core/init.rs
+++ src/core/init.rs
@@ -24,9 +24,27 @@ pub fn cmd_init(
         None => DEFAULT_PATH,
     };
 
+    if path == "-h" || path == "--help" {
+        return Ok(help().to_owned());
+    }
+
     let repo = GitRepository::create(Path::new(&path))?;
     Ok(format!(
         "initialized empty repository in {:?}\n",
         repo.worktree().as_os_str()
     ))
+}
+
+/// Display a help message for the init command
+const fn help() -> &'static str {
+    "mini_git init [path]
+  Initializes a new repository
+
+Options:
+  -h, --help: Display this help message
+
+Arguments:
+  path: The folder to initialize the new repository in,
+        defaults to the current working directory.
+        If provided, must be an existing directory."
 }
