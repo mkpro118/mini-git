a113af195fd46c1e82736d82aeb0e629ed07f863
M K <mkpro118@gmail.com>
2024-07-16 02:12:58 -0500

    Expose the refactored modules

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,5 @@
 #![deny(clippy::suspicious)]
 #![deny(unsafe_code)]
 
-pub mod configparser;
-pub mod datetime;
-pub mod fnmatch;
-pub mod sha1;
+pub mod utils;
 pub mod zlib;
---
+++ src/utils/mod.rs
@@ -0,0 +1,4 @@
+pub mod configparser;
+pub mod datetime;
+pub mod fnmatch;
+pub mod sha1;
