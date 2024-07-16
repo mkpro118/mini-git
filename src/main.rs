d44ee80d11cc3a2011f5b0f9f2ab717c54308237
M K <mkpro118@gmail.com>
2024-07-16 15:52:48 -0500

    Parse args and call subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use core::iter::Iterator;
+use std::env;
+
+use mini_git::core::init;
+
 fn main() {
     println!("Hello, world!");
+
+    let exit_code = run(env::args().skip(1));
+    std::process::exit(exit_code);
+}
+
+fn run(mut args: impl Iterator<Item = impl AsRef<str>>) -> i32 {
+    let Some(cmd) = args.next() else {
+        println!("Help!");
+        return 0;
+    };
+
+    let res = match cmd.as_ref() {
+        "init" => init::cmd_init(args),
+        _ => Ok("Help".to_string()),
+    };
+
+    match res {
+        Ok(msg) => {
+            println!("{msg}");
+            0
+        }
+        Err(msg) => {
+            println!("{msg}");
+            -1
+        }
+    }
 }
