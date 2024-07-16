cc03765ec382bd08018fff049da987af79f33ac2
M K <mkpro118@gmail.com>
2024-07-16 14:47:11 -0500

    impl init command

--- src/core/init.rs
+++ src/core/init.rs
@@ -1,1 +1,17 @@
+use crate::core::GitRepository;
+use std::path::Path;
 
+const DEFAULT_PATH: &str = ".";
+
+fn cmd_init(
+    args: impl IntoIterator<Item = impl AsRef<str>>,
+) -> Result<(), String> {
+    let path = args.into_iter().next();
+    let path = match path {
+        Some(ref arg) => arg.as_ref(),
+        None => DEFAULT_PATH,
+    };
+
+    let _ = GitRepository::create(&Path::new(&path))?;
+    Ok(())
+}
