5d778313d593b1a7dd5f24064f11dfe8d25c300a
M K <mkpro118@gmail.com>
2024-07-17 18:02:57 -0500

    Test auto compression

--- tests/test_zlib.rs
+++ tests/test_zlib.rs
@@ -1,13 +1,31 @@
-#![cfg(feature = "test-utils")]
-
 #[cfg(test)]
 mod tests {
-    use mini_git::utils::test::walkdir;
     use mini_git::zlib::{compress, compress::Strategy, decompress};
     use std::{
         fs,
         path::{Path, PathBuf},
     };
+
+    fn walkdir(top: &Path) -> Vec<PathBuf> {
+        assert!(top.is_dir(), "Top is not a directory (top = {top:?})");
+        top.read_dir()
+            .expect("Should read the dir")
+            .flatten()
+            .map(|e| e.path())
+            .filter(|path| {
+                path.file_stem().is_some_and(|stem| {
+                    !stem.to_str().is_some_and(|x| x.starts_with('.'))
+                })
+            })
+            .fold(vec![], |mut paths, entry| {
+                if entry.is_file() {
+                    paths.push(entry);
+                } else {
+                    paths.extend_from_slice(&walkdir(&entry));
+                }
+                paths
+            })
+    }
 
     #[test]
     fn test_fixed_on_license() {
@@ -57,16 +75,38 @@ mod tests {
         let src = root.join("src");
 
         for file in walkdir(&src) {
-            println!("{file:?}");
             let bytes = fs::read(file).expect("Read file!");
 
-            // Skip files that are too short,
-            // dynamic compression doesn't work well with them anyway
-            if bytes.len() < 100 {
-                continue;
-            }
-
             let compressed = compress(&bytes, &Strategy::Dynamic);
+            let decompressed =
+                decompress(&compressed).expect("Correct decompression");
+
+            assert_eq!(bytes, decompressed);
+        }
+    }
+
+    #[test]
+    fn test_auto_on_license() {
+        let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let license = root.join("LICENSE");
+        let bytes = fs::read(license).expect("Read file!");
+
+        let compressed = compress(&bytes, &Strategy::Auto);
+        let decompressed =
+            decompress(&compressed).expect("Correct decompression");
+
+        assert_eq!(bytes, decompressed);
+    }
+
+    #[test]
+    fn test_auto_on_source_files() {
+        let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let src = root.join("src");
+
+        for file in walkdir(&src) {
+            let bytes = fs::read(file).expect("Read file!");
+
+            let compressed = compress(&bytes, &Strategy::Auto);
             let decompressed =
                 decompress(&compressed).expect("Correct decompression");
 
