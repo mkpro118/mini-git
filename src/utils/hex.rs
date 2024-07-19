2c34f8db7b555ef9cb4324a06a1552e466b0245a
M K <mkpro118@gmail.com>
2024-07-19 02:44:27 -0500

    Add better error handling for hex encode/decode

--- src/utils/hex.rs
+++ src/utils/hex.rs
@@ -1,7 +1,23 @@
-pub fn decode_hex(s: &str) -> Result<Vec<u8>, std::num::ParseIntError> {
+use std::num::ParseIntError;
+
+pub enum DecodeHexError {
+    OddLength,
+    ParseInt(ParseIntError),
+}
+
+impl From<ParseIntError> for DecodeHexError {
+    fn from(e: ParseIntError) -> Self {
+        DecodeHexError::ParseInt(e)
+    }
+}
+
+pub fn decode_hex(s: &str) -> Result<Vec<u8>, DecodeHexError> {
+    if s.len() & 1 != 0 {
+        return Err(DecodeHexError::OddLength);
+    }
     (0..s.len())
         .step_by(2)
-        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16).map_err(|e| e.into()))
         .collect()
 }
 
