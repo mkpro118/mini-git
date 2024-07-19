fce027b5a380e91631f5a8ecf90f8c4c19266c22
M K <mkpro118@gmail.com>
2024-07-19 14:28:57 -0500

    Add panic message, improve formatting

--- tests/test_debug.rs
+++ tests/test_debug.rs
@@ -4,7 +4,7 @@ mod tests {
 
     #[test]
     fn test_datetime_to_str() {
-        let timestamp = 1704153600; // > January 1, 2024 in all timezones
+        let timestamp = 1_704_153_600; // > January 1, 2024 in all timezones
         let dt = DateTime::from_timestamp(timestamp);
         let str_repr = dt.to_str();
         dbg!(&dt);
@@ -17,8 +17,8 @@ mod tests {
 
     #[test]
     fn test_datetime_debug_impl() {
-        let dt = DateTime::from_timestamp(1609459200);
-        let debug_str = format!("{:?}", dt);
+        let dt = DateTime::from_timestamp(1_609_459_200);
+        let debug_str = format!("{dt:?}");
         assert!(debug_str.contains("time:"));
         assert!(debug_str.contains("tz:"));
     }
