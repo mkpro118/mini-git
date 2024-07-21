4dd59805c6c151397a6b9da70175267e22026c87
M K <mkpro118@gmail.com>
2024-07-20 23:42:36 -0500

    Macro to generate namespace maker

--- tests/core/mod.rs
+++ tests/core/mod.rs
@@ -1,2 +1,16 @@
 pub mod test_hash_object;
 pub mod test_init;
+
+#[macro_export]
+macro_rules! make_namespaces_from {
+    ($maker:ident) => {
+        fn make_namespaces<'a>(
+            args: &'a [&[&'a str]],
+        ) -> impl Iterator<Item = mini_git::utils::argparse::Namespace> + 'a {
+            let mut parser = $maker();
+            parser.compile();
+
+            args.iter().flat_map(move |&x| parser.parse_args(x))
+        }
+    };
+}
