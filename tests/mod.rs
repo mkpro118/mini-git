0b5ed9abfd3f1da2ea7e330392386b535813084d
M K <mkpro118@gmail.com>
2024-07-21 03:16:52 -0500

    Test mutex and exclusivity macro

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,2 +1,15 @@
 pub mod core;
 pub mod utils;
+
+pub static TEST_MUTEX: std::sync::Mutex<()> = std::sync::Mutex::new(());
+
+#[macro_export]
+macro_rules! with_test_mutex {
+    ($body:block) => {
+        if let Ok(_) = $crate::TEST_MUTEX.lock() {
+            $body
+        } else {
+            panic!("Test Mutex failed!");
+        }
+    };
+}
