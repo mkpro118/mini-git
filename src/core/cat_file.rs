9d8f40b910780f8c6a66cf893b3dc5ffc895ece0
M K <mkpro118@gmail.com>
2024-07-21 17:33:28 -0500

    Implement cat-file

--- src/core/cat_file.rs
+++ src/core/cat_file.rs
@@ -1,4 +1,8 @@
 use crate::utils::argparse::{ArgumentParser, ArgumentType, Namespace};
+use crate::utils::path;
+
+use crate::core::objects::{find_object, read_object};
+use crate::core::GitRepository;
 
 /// Provide content of repository objects
 /// This handles the subcommand
@@ -12,8 +16,22 @@ use crate::utils::argparse::{ArgumentParser, ArgumentType, Namespace};
 /// If file system operations fail, or if input paths are not valid.
 /// A [`String`] message describing the error is returned.
 #[allow(clippy::module_name_repetitions)]
-pub fn cmd_cat_file(_args: &Namespace) -> Result<String, String> {
-    todo!();
+pub fn cmd_cat_file(args: &Namespace) -> Result<String, String> {
+    let Ok(cwd) = std::env::current_dir() else {
+        return Err("Could not determined current working directory".to_owned());
+    };
+    let path = path::repo_find(cwd)?;
+    let repo = GitRepository::new(&path)?;
+
+    let obj_type = &args["type"];
+    let name = &args["object"];
+
+    let object = find_object(&repo, name, Some(obj_type), true);
+    let object = read_object(&repo, &object)?;
+    let Ok(s) = String::from_utf8(object.serialize()) else {
+        return Err("Failed to serialize object!".to_owned());
+    };
+    Ok(s)
 }
 
 /// Make `cat-file` parser
