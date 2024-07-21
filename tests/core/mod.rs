1f870412f963b29b3a608762d51c6a9f175b0b5b
M K <mkpro118@gmail.com>
2024-07-21 03:19:28 -0500

    Refactor test mutex to base of the crate

--- tests/core/mod.rs
+++ tests/core/mod.rs
@@ -14,16 +14,3 @@ macro_rules! make_namespaces_from {
         }
     };
 }
-
-pub static TEST_MUTEX: std::sync::Mutex<()> = std::sync::Mutex::new(());
-
-#[macro_export]
-macro_rules! with_test_mutex {
-    ($body:block) => {
-        if let Ok(_) = crate::core::TEST_MUTEX.lock() {
-            $body
-        } else {
-            panic!("Test Mutex failed!");
-        }
-    };
-}
