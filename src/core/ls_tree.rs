1f4f35e8ef8a49f1da3dd65486949841057411d6
M K <mkpro118@gmail.com>
2024-07-22 00:19:23 -0500

    ls-tree parser

--- src/core/ls_tree.rs
+++ src/core/ls_tree.rs
@@ -1,1 +1,35 @@
+use crate::utils::argparse::{ArgumentParser, ArgumentType, Namespace};
 
+/// Pretty-print a tree object.
+/// This handles the subcommand
+///
+/// ```bash
+/// mini_git ls-tree [--recursive] tree
+/// ```
+///
+/// # Errors
+///
+/// If file system operations fail, or if input paths are not valid.
+/// A [`String`] message describing the error is returned.
+#[allow(clippy::module_name_repetitions)]
+pub fn ls_tree(_args: &Namespace) -> Result<String, String> {
+    todo!()
+}
+
+/// Make `ls-tree` parser
+#[must_use]
+pub fn make_parser() -> ArgumentParser {
+    let mut parser = ArgumentParser::new("Pretty-print a tree object.");
+    parser
+        .add_argument("recursive", ArgumentType::Boolean)
+        .optional()
+        .short('r')
+        .add_help("Recurse into sub-trees");
+
+    parser
+        .add_argument("tree", ArgumentType::String)
+        .required()
+        .add_help("A tree-ish object.");
+
+    parser
+}
