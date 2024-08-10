e274eaf6e0a3ba3d9289f06378509b8e7cd0c89b
M K <mkpro118@gmail.com>
2024-08-10 16:49:59 -0700

    Tests for show-ref

--- tests/core/test_show_ref.rs
+++ tests/core/test_show_ref.rs
@@ -43,9 +43,12 @@ mod tests {
         let guard = FS_MUTEX.lock();
         match guard {
             Ok(mut inner) if inner.is_none() => {
+                println!("Allowing setup!");
                 *inner = Some(create_mock_repo());
             }
-            Ok(..) => {}
+            Ok(..) => {
+                println!("Bypassing setup!");
+            }
             Err(..) => panic!("Mutex failed!"),
         };
     }
@@ -53,6 +56,7 @@ mod tests {
     fn create_mock_repo() -> TempDir<'static, ()> {
         let tmp =
             TempDir::create("cmd_show_ref").with_mutex(&crate::TEST_MUTEX);
+        println!("temp = {:?}", tmp.tmp_dir());
         let _ = GitRepository::create(tmp.tmp_dir()).expect("Create repo");
         tmp.switch();
         tmp.run(|| {
@@ -63,9 +67,19 @@ mod tests {
         tmp
     }
 
+    fn create_dir_if_not_exists(path: &std::path::Path) {
+        if !path.exists() {
+            fs::create_dir_all(path)
+                .unwrap_or_else(|_| panic!("create dir {:?}", path));
+            return;
+        }
+        assert!(path.is_dir(), "Invalid test state!");
+    }
+
     fn create_heads() {
         let top = REFS_DIR().join("heads");
-        fs::create_dir(&top).expect("create dir");
+        create_dir_if_not_exists(&top);
+
         for res in [
             fs::write(top.join("main"), format!("{}\n", "0".repeat(40))),
             fs::write(top.join("feature1"), format!("{}\n", "1".repeat(40))),
@@ -77,13 +91,14 @@ mod tests {
 
     fn create_tags() {
         let top = REFS_DIR().join("tags");
-        fs::create_dir(&top).expect("create dir");
+        create_dir_if_not_exists(&top);
         let obj_top = OBJECT_DIR().join("ab");
-        fs::create_dir(&obj_top).expect("create dir");
+        create_dir_if_not_exists(&obj_top);
 
-        let tag_kvlm =
-            kvlm::KVLM::parse(format!("object {}", "12".repeat(20)).as_bytes())
-                .expect("parsed kvlm");
+        let tag_kvlm = kvlm::KVLM::parse(
+            format!("object {}\n\n", "12".repeat(20)).as_bytes(),
+        )
+        .expect("parsed kvlm");
         let tag = Tag::with_kvlm(tag_kvlm).serialize();
 
         for res in [
@@ -98,7 +113,7 @@ mod tests {
         let mut iter = ["5", "6", "7", "8"].into_iter();
         for top in ["origin", "develop"] {
             let top = REFS_DIR().join("remotes").join(top);
-            fs::create_dir_all(&top).expect("create dir");
+            create_dir_if_not_exists(&top);
 
             for res in [
                 fs::write(
@@ -118,5 +133,108 @@ mod tests {
     #[test]
     fn test_show_ref_basic() {
         setup();
+        let args: [&[&str]; 1] = [&[]];
+        let result = switch_dir!({
+            let namespace = make_namespaces(&args).next().unwrap();
+            dbg!(&namespace);
+            show_ref(&namespace)
+        });
+        assert!(result.is_ok());
+        let output = result.unwrap();
+        println!("OUTPUT=\n{:?}", &output);
+        assert!(output.contains("refs/heads/main"));
+        assert!(output.contains("refs/heads/feature1"));
+        assert!(output.contains("refs/heads/feature2"));
+        assert!(output.contains("refs/tags/v1"));
+        assert!(output.contains("refs/remotes/origin/main"));
+        assert!(output.contains("refs/remotes/origin/feature1"));
+        assert!(output.contains("refs/remotes/develop/main"));
+        assert!(output.contains("refs/remotes/develop/feature1"));
+    }
+
+    #[test]
+    fn test_show_ref_heads() {
+        setup();
+        let args: [&[&str]; 1] = [&["--heads"]];
+        let result = switch_dir!({
+            let namespace = make_namespaces(&args).next().unwrap();
+            show_ref(&namespace)
+        });
+        assert!(result.is_ok());
+        let output = result.unwrap();
+        assert!(output.contains("refs/heads/main"));
+        assert!(output.contains("refs/heads/feature1"));
+        assert!(output.contains("refs/heads/feature2"));
+        assert!(!output.contains("refs/tags/v1"));
+        assert!(!output.contains("refs/remotes/origin/main"));
+        assert!(!output.contains("refs/remotes/origin/feature1"));
+        assert!(!output.contains("refs/remotes/develop/main"));
+        assert!(!output.contains("refs/remotes/develop/feature1"));
+    }
+
+    #[test]
+    fn test_show_ref_tags() {
+        setup();
+        let args: [&[&str]; 1] = [&["--tags"]];
+        let result = switch_dir!({
+            let namespace = make_namespaces(&args).next().unwrap();
+            show_ref(&namespace)
+        });
+
+        assert!(result.is_ok());
+        let output = result.unwrap();
+        assert!(!output.contains("refs/heads/main"));
+        assert!(!output.contains("refs/heads/feature1"));
+        assert!(!output.contains("refs/heads/feature2"));
+        assert!(output.contains("refs/tags/v1"));
+        assert!(!output.contains("refs/remotes/origin/main"));
+        assert!(!output.contains("refs/remotes/origin/feature1"));
+        assert!(!output.contains("refs/remotes/develop/main"));
+        assert!(!output.contains("refs/remotes/develop/feature1"));
+    }
+
+    #[test]
+    fn test_show_ref_remotes() {
+        setup();
+        let args: [&[&str]; 1] = [&["--heads"]];
+        let result = switch_dir!({
+            let namespace = make_namespaces(&args).next().unwrap();
+            show_ref(&namespace)
+        });
+
+        assert!(result.is_ok());
+        let output = result.unwrap();
+        assert!(output.contains("refs/heads/main"));
+        assert!(output.contains("refs/heads/feature1"));
+        assert!(output.contains("refs/heads/feature2"));
+        assert!(!output.contains("refs/remotes/origin/main"));
+        assert!(!output.contains("refs/remotes/origin/feature1"));
+        assert!(!output.contains("refs/remotes/develop/main"));
+        assert!(!output.contains("refs/remotes/develop/feature1"));
+    }
+
+    #[test]
+    fn test_show_ref_exists() {
+        setup();
+        let args: [&[&str]; 1] = [&["--exists", "refs/heads/main"]];
+        let result = switch_dir!({
+            let namespace = make_namespaces(&args).next().unwrap();
+            show_ref(&namespace)
+        });
+
+        assert!(result.is_ok());
+        let output = result.unwrap();
+        assert!(output.is_empty()); // --exists flag implies no output if ref exists
+    }
+
+    #[test]
+    fn test_show_ref_not_exists() {
+        setup();
+        let args: [&[&str]; 1] = [&["--exists", "refs/heads/nonexistent"]];
+        let result = switch_dir!({
+            let namespace = make_namespaces(&args).next().unwrap();
+            show_ref(&namespace)
+        });
+        assert!(result.is_err());
     }
 }
