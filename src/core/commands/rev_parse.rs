bf0815c794d18175e939c49eabedf35b10eeb437
M K <mkpro118@gmail.com>
2024-11-06 16:19:03 +0530

    Add support for gitdir, refactor for devx

--- src/core/commands/rev_parse.rs
+++ src/core/commands/rev_parse.rs
@@ -2,6 +2,33 @@ use crate::core::{objects, GitRepository};
 use crate::utils::argparse::{ArgumentParser, ArgumentType, Namespace};
 use crate::utils::path;
 
+macro_rules! path_to_string {
+    ($path:expr, $err:literal) => {
+        match $path.to_str() {
+            Some(s) => Ok(String::from(s)),
+            None => Err($err.to_owned()),
+        }
+    };
+}
+
+type PathFunc = fn(&GitRepository) -> Result<String, String>;
+
+const OPTION_MAP: [(&str, PathFunc); 2] =
+    [("git-dir", gitdir), ("show-toplevel", show_toplevel)];
+
+/// List differences
+/// This handles the subcommand
+///
+/// ```bash
+/// mini_git rev-parse [--type TREE] [ --revision REVISION ]
+/// mini_git rev-parse --show-toplevel
+/// mini_git rev-parse --git-dir
+/// ```
+///
+/// # Errors
+///
+/// If file system operations fail, or if input paths are not valid.
+/// A [`String`] message describing the error is returned.
 #[allow(clippy::module_name_repetitions)]
 pub fn rev_parse(args: &Namespace) -> Result<String, String> {
     let cwd = std::env::current_dir().map_err(|_| {
@@ -13,17 +40,42 @@ pub fn rev_parse(args: &Namespace) -> Result<String, String> {
         .map_err(|_| "Could not determine repository path".to_owned())?;
     let repo = GitRepository::new(&repo_path)?;
 
-    if args.get("show-toplevel").is_some() {
-        return repo_path
-            .into_os_string()
-            .into_string()
-            .map_err(|_| "Could not determine repository toplevel".to_owned());
+    let mut output = String::new();
+
+    for option in &args.order {
+        let Ok(res) = OPTION_MAP
+            .binary_search_by(|opt| opt.0.cmp(option))
+            .map(|x| (OPTION_MAP[x].1)(&repo))
+        else {
+            continue;
+        };
+        let Ok(res) = res else {
+            return res;
+        };
+        output.push_str(&res);
+        output.push('\n');
     }
 
-    let type_ = args.get("type").map(|x| x.as_str());
+    let type_ = args.get("type").map(std::string::String::as_str);
     let revision = &args["revision"];
 
-    objects::find_object(&repo, revision, type_, true)
+    if revision == "*" {
+        return Ok(output);
+    }
+
+    let res = objects::find_object(&repo, revision, type_, true)?;
+
+    output.push_str(&res);
+    output.push('\n');
+    Ok(output)
+}
+
+fn show_toplevel(repo: &GitRepository) -> Result<String, String> {
+    path_to_string!(repo.worktree(), "Could not determine repository toplevel")
+}
+
+fn gitdir(repo: &GitRepository) -> Result<String, String> {
+    path_to_string!(repo.gitdir(), "Could not determine repository gitdir")
 }
 
 /// Make `rev-parse` parser
@@ -43,6 +95,10 @@ pub fn make_parser() -> ArgumentParser {
     );
 
     parser
+        .add_argument("git-dir", ArgumentType::Boolean)
+        .add_help("Show the absolute path to the .git directory.");
+
+    parser
         .add_argument("revision", ArgumentType::String)
         .required()
         .default("*")
@@ -50,3 +106,48 @@ pub fn make_parser() -> ArgumentParser {
 
     parser
 }
+
+// The following code ensures that the Command array is sorted at compile time.
+// The Command array is required to be sorted to be binary-search friendly,
+// and we enforce this at compile time.
+#[allow(dead_code)]
+#[inline]
+const fn str_le(a: &'static str, b: &'static str) -> bool {
+    let (a, b) = (a.as_bytes(), b.as_bytes());
+    let mut i = 0;
+    let len = if a.len() < b.len() { a.len() } else { b.len() };
+
+    while i < len {
+        if a[i] < b[i] {
+            return true;
+        } else if a[i] > b[i] {
+            return false;
+        }
+        i += 1;
+    }
+    len == a.len()
+}
+
+#[allow(dead_code)]
+#[inline]
+const fn is_map_sorted() -> bool {
+    let len = OPTION_MAP.len();
+    assert!(len > 1, "OPTIONA MAP IS EMPTY");
+    let mut prev_name = &OPTION_MAP[0].0;
+    let mut i = 1;
+
+    while i < len {
+        if !str_le(prev_name, OPTION_MAP[i].0) {
+            return false;
+        }
+
+        prev_name = &OPTION_MAP[0].0;
+        i += 1;
+    }
+
+    true
+}
+
+// If this fails to compile, the command array is not sorted
+#[allow(clippy::erasing_op)]
+const _: u8 = 0 / is_map_sorted() as u8;
