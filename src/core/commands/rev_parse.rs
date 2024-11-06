ca7dea5ceed2d59da79cc36cdefcda08815ed8e8
M K <mkpro118@gmail.com>
2024-11-06 14:37:53 +0530

    make the `rev-parse` parser

--- src/core/commands/rev_parse.rs
+++ src/core/commands/rev_parse.rs
@@ -1,1 +1,24 @@
+use crate::utils::argparse::{ArgumentParser, ArgumentType, Namespace};
 
+#[allow(clippy::module_name_repetitions)]
+pub fn rev_parse(_args: &Namespace) -> Result<String, String> {
+    todo!()
+}
+
+/// Make `rev-parse` parser
+#[must_use]
+pub fn make_parser() -> ArgumentParser {
+    let mut parser =
+        ArgumentParser::new("Parse revision (or other objects) identifiers");
+    parser
+        .add_argument("type", ArgumentType::String)
+        .choices(&["blob", "commit", "tag", "tree"])
+        .add_help("Specify the type of object");
+
+    parser
+        .add_argument("revision", ArgumentType::String)
+        .required()
+        .add_help("The revision to parse");
+
+    parser
+}
