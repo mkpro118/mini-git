03f420ac1432650222f52c3226775e2b329191f6
M K <mkpro118@gmail.com>
2024-11-10 02:14:27 +0530

    Refactor file list resolution

--- src/core/commands/diff.rs
+++ src/core/commands/diff.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet};
+use std::path::Path;
 use std::sync::Arc;
 use std::thread;
 
@@ -86,51 +87,12 @@ pub fn diff(args: &Namespace) -> Result<String, String> {
     };
 
     // Resolve the file paths to be relative to the repository root
-    let mut resolved_files: Vec<String> = vec![];
-    for file in &args
-        .get("files")
-        .map(|f| f.split(',').collect::<Vec<_>>())
-        .unwrap_or_default()
-    {
-        // Create a path by joining the current working directory with the file path
-        let file_path = cwd.join(file);
-
-        // Canonicalize the path to get the absolute path
-        let abs_path = file_path
-            .canonicalize()
-            .map_err(|_| format!("Could not canonicalize path {file}"))?;
-
-        if !abs_path.exists() {
-            return Err(format!("File {file} does not exist in the worktree"));
-        }
-
-        if abs_path.is_file() {
-            // Get the relative path from the repository root to the file
-            let rel_path = abs_path.strip_prefix(&repo_path).map_err(|_| {
-                format!("Could not get path relative to repo root for {file}")
-            })?;
-
-            // Convert the relative path to a string and store it
-            resolved_files.push(rel_path.to_string_lossy().to_string());
-        } else if abs_path.is_dir() {
-            // Get all files under this directory
-            let worktree_files =
-                worktree::get_worktree_files(&repo, Some(&abs_path))?;
-            for worktree_file in worktree_files {
-                // worktree_file is relative to abs_path, so we need to get the absolute path
-                let file_abs_path = abs_path.join(&worktree_file);
-
-                // Get the relative path from the repository root
-                let rel_path = file_abs_path.strip_prefix(&repo_path).map_err(|_| {
-                    format!("Could not get path relative to repo root for {file}")
-                })?;
-
-                resolved_files.push(rel_path.to_string_lossy().to_string());
-            }
-        } else {
-            return Err(format!("{file} is neither a file nor a directory"));
-        }
-    }
+    let all_files = repo_path.to_str().map_or_else(
+        || Err("Failed to determined files to diff".to_owned()),
+        |x| Ok(String::from(x)),
+    );
+    let files = args.get("files").map(Ok).unwrap_or(all_files.as_ref())?;
+    let resolved_files: Vec<String> = resolve_cla_files(&repo, &cwd, files)?;
 
     // Create a Vec<&str> from the adjusted file paths
     let files: Vec<&str> = resolved_files.iter().map(String::as_str).collect();
@@ -167,6 +129,60 @@ fn _diff(
     let all_files = collect_files_to_process(&files1, &files2, &opts.files);
 
     process_files_in_parallel(files1, files2, &all_files, opts)
+}
+
+// Resolves files passed in on the command line
+fn resolve_cla_files(
+    repo: &GitRepository,
+    cwd: &Path,
+    files: &str,
+) -> Result<Vec<String>, String> {
+    let mut resolved_files = vec![];
+    for file in files.split(',') {
+        // Create a path by joining the current working directory with the file path
+        let file_path = cwd.join(file);
+
+        // Canonicalize the path to get the absolute path
+        let abs_path = file_path
+            .canonicalize()
+            .map_err(|_| format!("Could not canonicalize path {file}"))?;
+
+        if !abs_path.exists() {
+            return Err(format!("File {file} does not exist in the worktree"));
+        }
+
+        if abs_path.is_file() {
+            // Get the relative path from the repository root to the file
+            let rel_path =
+                abs_path.strip_prefix(repo.worktree()).map_err(|_| {
+                    format!(
+                        "Could not get path relative to repo root for {file}"
+                    )
+                })?;
+
+            // Convert the relative path to a string and store it
+            resolved_files.push(rel_path.to_string_lossy().to_string());
+        } else if abs_path.is_dir() {
+            // Get all files under this directory
+            let worktree_files =
+                worktree::get_worktree_files(repo, Some(&abs_path))?;
+            for worktree_file in worktree_files {
+                // worktree_file is relative to abs_path, so we need to get the absolute path
+                let file_abs_path = abs_path.join(&worktree_file);
+
+                // Get the relative path from the repository root
+                let rel_path = file_abs_path.strip_prefix(repo.worktree()).map_err(|_| {
+                    format!("Could not get path relative to repo root for {file}")
+                })?;
+
+                resolved_files.push(rel_path.to_string_lossy().to_string());
+            }
+        } else {
+            return Err(format!("{file} is neither a file nor a directory"));
+        }
+    }
+
+    Ok(resolved_files)
 }
 
 // Resolves the tree references based on input parameters
