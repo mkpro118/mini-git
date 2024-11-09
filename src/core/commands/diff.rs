af5636e23c5416282c2780a600eedcd3161f6b49
M K <mkpro118@gmail.com>
2024-11-09 17:40:44 +0530

    Test multiple deletions

--- src/core/commands/diff.rs
+++ src/core/commands/diff.rs
@@ -1233,6 +1233,18 @@ mod tests {
     }
 
     #[test]
+    fn test_compute_diff_with_multiple_deletions() {
+        let old_lines = ["Line 1", "Line 2", "Line 3", "Line 4"];
+        let new_lines = ["Line 1", "Line 4"];
+        let changes = compute_diff(&old_lines, &new_lines);
+        assert_eq!(changes.len(), 4);
+        assert_eq!(changes[0], Change::Same);
+        assert_eq!(changes[1], Change::Delete);
+        assert_eq!(changes[2], Change::Delete);
+        assert_eq!(changes[3], Change::Same);
+    }
+
+    #[test]
     fn test_generate_hunks_simple_change() {
         let old_lines = ["Line 1", "Line 2", "Line 3"];
         let new_lines = ["Line 1", "Changed Line 2", "Line 3"];
