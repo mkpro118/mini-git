35d390ed52eeba6b1da8a33ef5fc98f0e84cfddb
M K <mkpro118@gmail.com>
2025-05-24 23:33:38 -0700

    Apply the clippy rules from src to tests

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,3 +1,13 @@
+#![forbid(clippy::complexity)]
+#![forbid(clippy::correctness)]
+#![forbid(clippy::perf)]
+#![forbid(clippy::suspicious)]
+#![forbid(clippy::allow_attributes)]
+#![forbid(future_incompatible)]
+#![deny(clippy::pedantic)]
+#![deny(clippy::style)]
+#![deny(unsafe_code)]
+
 pub mod core;
 pub mod utils;
 
