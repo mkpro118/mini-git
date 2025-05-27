5f753aa7201ecd2c64ce7004122c924a49d138d1
M K <mkpro118@gmail.com>
2025-05-27 00:37:44 -0700

    Add function to resolve a path relative to the top level of worktree

--- src/utils/path.rs
+++ src/utils/path.rs
@@ -25,6 +25,8 @@
 use std::fs;
 use std::path::{Path, PathBuf};
 
+use crate::core::GitRepository;
+
 const POSIX_PATH_SEPARATOR: char = '/';
 const CURRENT_DIR_STR: &str = ".";
 const PARENT_DIR_STR: &str = "..";
@@ -342,6 +344,18 @@ where
         "neither {} nor any of it's parent directories is a repository.",
         top.display()
     ))
+}
+
+/// Returns the relative path from `repo` to `absolute_path`
+/// Returns `None` if `absolute_path` is not in `repo`
+pub fn repo_relative_path(
+    repo: &GitRepository,
+    absolute_path: &Path,
+) -> Option<PathBuf> {
+    absolute_path
+        .strip_prefix(repo.worktree())
+        .ok()
+        .map(Path::to_path_buf)
 }
 
 #[cfg(test)]
